621c96819da96a885f52394f8026a0da9bc1d6b5
nuruddin2971 <nuruddin2971@gmail.com>
2026-08-20 22:23:45 +0600

    first commit

---
+++ dist/getTicketPrice.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const getTicketPrice = (age) => {
+    if (age < 5) {
+        return 0;
+    }
+    else if (age >= 5 && age <= 12) {
+        return 100;
+    }
+    else if (age >= 13 && age <= 59) {
+        return 200;
+    }
+    return 120;
+};
+getTicketPrice(3);
+getTicketPrice(10);
+getTicketPrice(25);
+getTicketPrice(65);
+//# sourceMappingURL=getTicketPrice.js.map
