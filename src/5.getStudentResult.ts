2f355c1ff73b99ae48ac59992f98ad223f0a5e46
nuruddin2971 <nuruddin2971@gmail.com>
2026-08-21 14:41:48 +0600

    solved student result

---
+++ src/5.getStudentResult.ts
@@ -0,0 +1,35 @@
+interface Student {
+  name: string;
+  marks: number[];
+}
+
+interface Result {
+  name: string;
+  average: number;
+  result: string;
+}
+
+const getStudentResult = (student: Student): Result => {
+  const marks: number[] = student.marks;
+  const totalMarks: number = marks.reduce((sum, mark) => sum + mark, 0);
+  const average: number = totalMarks / marks.length;
+  const result: string = average >= 40 ? "Passed" : "Faild";
+  return {
+    name: student.name,
+    average,
+    result
+  };
+};
+console.log(
+  getStudentResult({
+    name: "Rafi",
+    marks: [80, 75, 90, 85],
+  }),
+);
+
+console.log(
+  getStudentResult({
+    name: "Nabil",
+    marks: [30, 35, 40, 25],
+  }),
+);
